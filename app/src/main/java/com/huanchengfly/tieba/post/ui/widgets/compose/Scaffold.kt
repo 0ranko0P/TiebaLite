a39c331e7715672c1d93dbaa38d2c7db8b39560f
0ranko0P <ranko0p@outlook.com>
2024-10-29 17:37:13 +0800

    fix: MyScaffold unaware of system navigation bar

--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Scaffold.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Scaffold.kt
@@ -1,7 +1,13 @@
 package com.huanchengfly.tieba.post.ui.widgets.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material.DismissValue
 import androidx.compose.material.DrawerDefaults
 import androidx.compose.material.ExperimentalMaterialApi
@@ -23,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import com.huanchengfly.tieba.post.ui.common.theme.compose.LocalExtendedColors
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -48,6 +55,17 @@ private fun SwipeToDismissSnackbarHost(hostState: SnackbarHostState) {
 }
 
 val LocalSnackbarHostState = compositionLocalOf<SnackbarHostState> { error("no scaffold here!") }
+
+// Placeholder to make MyScaffold properly offset NavBar
+// since we are enabling Edge-To-Edge everywhere
+val NavigationBarPlaceHolder: @Composable () -> Unit = {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .windowInsetsBottomHeight(WindowInsets.navigationBars)
+            .background(LocalExtendedColors.current.bottomBar)
+    )
+}
 
 @Composable
 fun MyScaffold(
