425f8d8aa175d666f1032d9c811a06470133f10b
0ranko0P <ranko0p@outlook.com>
2024-10-29 21:59:30 +0800

    feat: Refactor ErrorTipScreen

--- app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/exception/TiebaNotLoggedInException.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/exception/TiebaNotLoggedInException.kt
@@ -2,10 +2,8 @@ package com.huanchengfly.tieba.post.api.retrofit.exception
 
 import com.huanchengfly.tieba.post.api.Error.ERROR_NOT_LOGGED_IN
 
-class TiebaNotLoggedInException(
-    msg: String = "Not logged in!"
-) : TiebaLocalException(ERROR_NOT_LOGGED_IN, msg) {
+class TiebaNotLoggedInException : TiebaLocalException(ERROR_NOT_LOGGED_IN, "") {
     override fun toString(): String {
-        return "TiebaNotLoggedInException(message=$message)"
+        return "TiebaNotLoggedInException"
     }
 }
--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Errors.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Errors.kt
@@ -1,10 +1,16 @@
 package com.huanchengfly.tieba.post.ui.widgets.compose
 
+import android.content.Context
+import androidx.annotation.RawRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidthIn
 import androidx.compose.foundation.rememberScrollState
@@ -13,12 +19,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -34,6 +42,8 @@ import com.huanchengfly.tieba.post.arch.BaseComposeActivity.Companion.LocalWindo
 import com.huanchengfly.tieba.post.ui.common.theme.compose.ExtendedTheme
 import com.huanchengfly.tieba.post.ui.common.windowsizeclass.WindowWidthSizeClass
 import com.huanchengfly.tieba.post.ui.widgets.compose.states.StateScreenScope
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 @Composable
 fun TipScreen(
@@ -48,8 +58,8 @@ fun TipScreen(
         if (scrollable) Modifier.verticalScroll(rememberScrollState()) else Modifier
     val widthFraction =
         if (LocalWindowSizeClass.current.widthSizeClass == WindowWidthSizeClass.Compact) 0.9f else 0.5f
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        contentAlignment = Alignment.TopCenter,
         modifier = modifier
     ) {
         Column(
@@ -60,14 +70,13 @@ fun TipScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.CenterVertically)
         ) {
-            Column(modifier = Modifier.requiredWidthIn(max = 400.dp)) {
-                image()
+            Box(modifier = Modifier.requiredWidthIn(max = 400.dp)) {
+                this@Column.image()
             }
             ProvideTextStyle(
                 value = MaterialTheme.typography.h6.copy(
                     color = ExtendedTheme.colors.text,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
+                    fontWeight = FontWeight.Bold
                 )
             ) {
                 title()
@@ -75,7 +84,6 @@ fun TipScreen(
             ProvideTextStyle(
                 value = MaterialTheme.typography.body1.copy(
                     color = ExtendedTheme.colors.textSecondary,
-                    textAlign = TextAlign.Center
                 )
             ) {
                 message()
@@ -85,13 +93,13 @@ fun TipScreen(
     }
 }
 
-enum class ErrorType {
-    NETWORK,
-    SERVER,
-    NOT_LOGGED_IN,
-    UNKNOWN
-}
+data class ErrorType(
+    @StringRes val title: Int,
+    val message: String,
+    @RawRes val lottieResId: Int,
+)
 
+@NonRestartableComposable
 @Composable
 fun StateScreenScope.ErrorScreen(
     error: Throwable?,
@@ -99,151 +107,121 @@ fun StateScreenScope.ErrorScreen(
     showReload: Boolean = true,
     actions: @Composable (ColumnScope.() -> Unit) = {},
 ) {
-    error?.let {
-        ErrorTipScreen(
-            error = it,
-            modifier = modifier,
-            actions = {
-                if (showReload && canReload) {
-                    Button(onClick = { reload() }) {
-                        Text(text = stringResource(id = R.string.btn_reload))
-                    }
-                }
-
-                actions()
-            }
-        )
-    }
-}
-
-@Composable
-fun ErrorTipScreen(
-    error: Throwable,
-    modifier: Modifier = Modifier,
-    actions: @Composable (ColumnScope.() -> Unit) = {},
-) {
-    val errorType = when (error) {
-        is NoConnectivityException -> ErrorType.NETWORK
-        is TiebaApiException -> ErrorType.SERVER
-        is TiebaNotLoggedInException -> ErrorType.NOT_LOGGED_IN
-        else -> ErrorType.UNKNOWN
-    }
-    val errorMessage = error.getErrorMessage()
-    val errorCode = error.getErrorCode()
     ErrorTipScreen(
-        errorType = errorType,
-        errorMessage = errorMessage,
+        error = error,
         modifier = modifier,
-        errorCode = errorCode,
-        actions = actions
+        actions = {
+            if (showReload && canReload) {
+                Button(onClick = { reload() }) {
+                    Text(text = stringResource(id = R.string.btn_reload))
+                }
+            }
+            actions()
+        }
     )
 }
 
 @Composable
 fun ErrorTipScreen(
-    errorType: ErrorType,
-    errorMessage: String,
     modifier: Modifier = Modifier,
-    errorCode: Int? = null,
-    appendMessage: @Composable (ColumnScope.() -> Unit) = {},
+    error: Throwable?,
     actions: @Composable (ColumnScope.() -> Unit) = {},
 ) {
+    val context = LocalContext.current
+    val errorType = remember { toKnownErrorType(context, error) }
+    // Is unknown error, show stack trace
+    if (errorType == null) {
+        ErrorStackTraceScreen(modifier, error!!, actions)
+        return
+    }
+
     TipScreen(
         title = {
-            when (errorType) {
-                ErrorType.NETWORK -> {
-                    Text(text = stringResource(id = R.string.title_no_internet_connectivity))
-                }
-
-                ErrorType.SERVER -> {
-                    Text(text = stringResource(id = R.string.title_api_error))
-                }
-
-                ErrorType.UNKNOWN -> {
-                    Text(text = stringResource(id = R.string.title_unknown_error))
-                }
-
-                ErrorType.NOT_LOGGED_IN -> {
-                    Text(text = stringResource(id = R.string.title_not_logged_in))
-                }
-            }
+            Text(text = stringResource(id = errorType.title))
         },
         modifier = modifier,
         image = {
-            when (errorType) {
-                ErrorType.NETWORK -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_no_internet))
-                    LottieAnimation(
-                        composition = composition,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(2f)
-                    )
-                }
-
-                ErrorType.SERVER -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_error))
-                    LottieAnimation(
-                        composition = composition,
-                        iterations = LottieConstants.IterateForever,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(2f)
-                    )
-                }
-
-                ErrorType.UNKNOWN -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_bug_hunting))
-                    LottieAnimation(
-                        composition = composition,
-                        iterations = LottieConstants.IterateForever,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(2f)
-                    )
-                }
-
-                ErrorType.NOT_LOGGED_IN -> {
-                    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.lottie_astronaut))
-                    LottieAnimation(
-                        composition = composition,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .aspectRatio(2f)
-                    )
-                }
-            }
+            val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(errorType.lottieResId))
+            LottieAnimation(
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2f)
+            )
         },
         message = {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                when (errorType) {
-                    ErrorType.NETWORK -> {
-                        Text(
-                            text = stringResource(
-                                id = R.string.message_no_internet_connectivity,
-                                errorMessage
-                            )
-                        )
-                    }
-
-                    ErrorType.SERVER -> {
-                        val errorCodeText = "($errorCode)".takeIf { errorCode != null }.orEmpty()
-                        Text(text = "$errorMessage$errorCodeText")
-                    }
-
-                    ErrorType.UNKNOWN -> {
-                        Text(text = stringResource(id = R.string.message_unknown_error))
-                    }
-
-                    ErrorType.NOT_LOGGED_IN -> {
-                        Text(text = stringResource(id = R.string.message_not_logged_in))
-                    }
-                }
-                appendMessage()
-            }
+            Text(text = errorType.message)
         },
         actions = actions
     )
+}
+
+@Composable
+fun ErrorStackTraceScreen(
+    modifier: Modifier = Modifier,
+    throwable: Throwable,
+    actions: @Composable (ColumnScope.() -> Unit) = {}
+) {
+    val stackTrace = remember { runCatching {
+        val bos = ByteArrayOutputStream()
+        PrintStream(bos).use { out ->
+            throwable.printStackTrace(out)
+            bos.toString()
+        }
+    } }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp, alignment = Alignment.CenterVertically)
+    ) {
+        Text(
+            text = stringResource(id = R.string.title_unknown_error),
+            style = MaterialTheme.typography.h6
+        )
+
+        Text(
+            text = stackTrace.getOrDefault(stringResource(R.string.message_unknown_error)),
+            modifier = Modifier
+                .heightIn(max = 700.dp)
+                .verticalScroll(rememberScrollState())
+                .horizontalScroll(rememberScrollState())
+            ,
+            color = ExtendedTheme.colors.textSecondary,
+            softWrap = false,
+            style = MaterialTheme.typography.body1
+        )
+
+        actions()
+    }
+}
+
+// Returns null if error is unknown type
+private fun toKnownErrorType(context: Context, err: Throwable?): ErrorType? {
+    return when (err) {
+        null -> ErrorType(R.string.title_unknown_error, context.getString(R.string.message_unknown_error), R.raw.lottie_bug_hunting)
+
+        is NoConnectivityException -> ErrorType(
+            title = R.string.title_no_internet_connectivity,
+            message = context.getString(R.string.message_no_internet_connectivity, err.getErrorMessage()),
+            lottieResId = R.raw.lottie_no_internet
+        )
+
+        is TiebaApiException -> ErrorType(
+            title = R.string.title_api_error,
+            message = "${err.getErrorMessage()} Code: ${err.getErrorCode()}",
+            lottieResId = R.raw.lottie_error
+        )
+
+        is TiebaNotLoggedInException -> ErrorType(
+            title = R.string.title_not_logged_in,
+            message = context.getString(R.string.message_unknown_error),
+            lottieResId = R.raw.lottie_astronaut
+        )
+
+        else -> null // Unknown Type
+    }
 }
