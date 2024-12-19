8d272b73b86a0cd3b02c158434dbde52266354b9
0ranko0P <ranko0p@outlook.com>
2024-12-19 20:37:10 +0800

    feat: Introduce VideoViewActivity

---
+++ app/src/main/java/com/huanchengfly/tieba/post/activities/VideoViewActivity.kt
@@ -0,0 +1,83 @@
+package com.huanchengfly.tieba.post.activities
+
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.huanchengfly.tieba.post.api.models.protos.VideoInfo
+import com.huanchengfly.tieba.post.goToActivity
+import com.huanchengfly.tieba.post.ui.widgets.compose.video.OnFullScreenModeChangedListener
+import com.huanchengfly.tieba.post.ui.widgets.compose.video.VideoPlayer
+import com.huanchengfly.tieba.post.ui.widgets.compose.video.VideoPlayerController
+import com.huanchengfly.tieba.post.ui.widgets.compose.video.VideoPlayerSource
+import com.huanchengfly.tieba.post.ui.widgets.compose.video.rememberVideoPlayerController
+
+class VideoViewActivity: ComponentActivity(), OnFullScreenModeChangedListener {
+
+    private val mInsetsController by lazy { WindowCompat.getInsetsController(window, window.decorView) }
+
+    private var videoPlayerController: VideoPlayerController? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        val data = intent.data ?: throw NullPointerException("No video provided!")
+        val thumbnailUrl = intent.getStringExtra(EXTRA_THUMBNAIL)
+
+        setContent {
+            videoPlayerController = rememberVideoPlayerController(
+                source = VideoPlayerSource.Network(data.toString()),
+                thumbnailUrl = thumbnailUrl,
+                fullScreenModeChangedListener = this
+            )
+            VideoPlayer(
+                videoPlayerController = videoPlayerController!!,
+                modifier = Modifier.fillMaxSize(),
+                backgroundColor = Color.Black
+            )
+
+            LaunchedEffect(Unit) {
+                videoPlayerController?.play()
+            }
+        }
+    }
+
+    override fun onFullScreenModeChanged(isFullScreen: Boolean) {
+        if (isFullScreen) {
+            mInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        } else {
+            mInsetsController.show(WindowInsetsCompat.Type.systemBars())
+            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
+
+    companion object {
+        const val EXTRA_THUMBNAIL = "video_thumbnail"
+
+        fun launch(context: Context, videoUrl: String, thumbnailUrl: String?) {
+            // Free more memory now
+            Glide.get(context).clearMemory()
+
+            context.goToActivity<VideoViewActivity> {
+                data = Uri.parse(videoUrl)
+                thumbnailUrl?.let { putExtra(EXTRA_THUMBNAIL, it) }
+            }
+        }
+
+        fun launch(context: Context, videoInfo: VideoInfo) {
+            launch(context, videoInfo.videoUrl, videoInfo.thumbnailUrl)
+        }
+    }
+}
