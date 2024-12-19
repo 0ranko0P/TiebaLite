037f8ee456adf41a03f60dba823e62a868529d07
0ranko0P <ranko0p@outlook.com>
2024-12-19 19:29:36 +0800

    chore: Avoid recompositions when animating MediaControlButton background

--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/video/MediaContaonButtons.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/video/MediaContaonButtons.kt
@@ -1,9 +1,8 @@
 package com.huanchengfly.tieba.post.ui.widgets.compose.video
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.IconButton
@@ -12,61 +11,28 @@ import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Restore
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 
 @Composable
 fun MediaControlButtons(
     modifier: Modifier = Modifier
 ) {
     val controller = LocalVideoPlayerController.current
-
     val controlsEnabled by controller.collect { controlsEnabled }
 
-    // Dictates the direction of appear animation.
-    // If controlsVisible is true, appear animation needs to be triggered.
-    val controlsVisible by controller.collect { controlsVisible }
-
-    // When controls are not visible anymore we should remove them from UI tree
-    // Controls by default should always be on screen.
-    // Only when disappear animation finishes, controls can be freely cleared from the tree.
-    val (controlsExistOnUITree, setControlsExistOnUITree) = remember(controlsVisible) {
-        mutableStateOf(true)
-    }
-
-    val appearAlpha = remember { Animatable(0f) }
-
-    LaunchedEffect(controlsVisible) {
-        appearAlpha.animateTo(
-            targetValue = if (controlsVisible) 1f else 0f,
-            animationSpec = tween(
-                durationMillis = 250,
-                easing = LinearEasing
-            )
-        )
-        setControlsExistOnUITree(controlsVisible)
-    }
-
-    if (controlsEnabled && controlsExistOnUITree) {
-        MediaControlButtonsContent(
-            modifier = Modifier
-                .alpha(appearAlpha.value)
-                .background(Color.Black.copy(alpha = appearAlpha.value * 0.6f))
-                .then(modifier)
-        )
-    }
-}
-
-@Composable
-private fun MediaControlButtonsContent(modifier: Modifier = Modifier) {
-    Box(modifier = modifier) {
-        PlayPauseButton(modifier = Modifier.align(Alignment.Center))
+    if (controlsEnabled) {
+        Box(
+            modifier = modifier.buttonVisibility(controller),
+            contentAlignment = Alignment.Center
+        ) {
+            PlayPauseButton()
+        }
     }
 }
 
@@ -99,4 +65,27 @@ fun PlayPauseButton(modifier: Modifier = Modifier) {
             }
         }
     }
+}
+
+// Same with Modifier.alpha() + Modifier.background(), but background is
+// animated, draw manually to avoid unnecessary recompositions
+private fun Modifier.buttonVisibility(controller: DefaultVideoPlayerController) = this then composed {
+    // Dictates the direction of appear animation.
+    // If controlsVisible is true, appear animation needs to be triggered.
+    val controlsVisible by controller.collect { controlsVisible }
+
+    val appearAlpha by animateFloatAsState(
+        targetValue = if (controlsVisible) 1f else 0f,
+        animationSpec = tween(durationMillis = 250, easing = LinearEasing),
+        label = "ControlButtonAlphaAnimation"
+    )
+
+    Modifier
+        .drawWithCache {
+            onDrawBehind { drawRect(Color.Black.copy(appearAlpha * 0.6f), size = size) }
+        }
+        .graphicsLayer {
+            alpha = appearAlpha
+            translationY = if (alpha == 0f) size.height else 0f
+        }
 }
