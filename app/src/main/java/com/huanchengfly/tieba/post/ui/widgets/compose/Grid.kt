918181262daedda2f415a51aedc7b650c1765821
0ranko0P <ranko0p@outlook.com>
2025-09-29 22:44:00 +0800

    feat: Optmize VerticalGrid

--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Grid.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/Grid.kt
@@ -4,13 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.movableContentWithReceiverOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.util.fastMap
 
 interface GridScope {
     fun item(
@@ -81,30 +82,6 @@ class GridCounter(
     }
 }
 
-private fun calcRows(column: Int, items: List<GridScopeImpl.Item>): List<List<GridScopeImpl.Item>> {
-    val rows = mutableListOf<List<GridScopeImpl.Item>>()
-    val allItems = items.toMutableList()
-    while (allItems.isNotEmpty()) {
-        var columnCount = column
-        val row = mutableListOf<GridScopeImpl.Item>()
-        while (allItems.isNotEmpty() && columnCount > 0) {
-            val item = allItems[0]
-            check(item.span <= columnCount || item.span <= column)
-            if (item.span > columnCount) {
-                break
-            }
-            columnCount -= item.span
-            row.add(item)
-            allItems.removeAt(0)
-        }
-        if (allItems.isEmpty() && columnCount > 0) {
-            row.add(GridScopeImpl.Item(columnCount) { Spacer(modifier = Modifier.fillMaxWidth()) })
-        }
-        rows.add(row)
-    }
-    return rows
-}
-
 @Composable
 fun VerticalGrid(
     column: Int,
@@ -115,21 +92,31 @@ fun VerticalGrid(
     content: GridScope.() -> Unit
 ) {
     val gridScope = remember(content) { GridScopeImpl().apply(content) }
+    val items = gridScope.items
+    val movableItems = remember(gridScope) {
+        items.fastMap {
+            movableContentWithReceiverOf<RowScope> {
+                Box(
+                    modifier = Modifier.weight(it.span.toFloat()),
+                    contentAlignment = Alignment.Center,
+                    content = { it.content() }
+                )
+            }
+        }
+    }
+
+    val rows = items.size / column + (if (items.size % column == 0) 0 else 1)
     Column(modifier = modifier, verticalArrangement = verticalArrangement) {
-        val rows = remember(column, gridScope) { calcRows(column, gridScope.items) }
-        rows.forEach {
+        repeat(rows) { row ->
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = horizontalArrangement,
                 modifier = rowModifier
             ) {
-                it.forEach {
-                    Box(
-                        modifier = Modifier.weight(it.span.toFloat()),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        it.content()
-                    }
+                val start = row * column
+                val end = minOf(start + column, items.size)
+                for (index in start until end) {
+                    movableItems[index](this)
                 }
             }
         }
