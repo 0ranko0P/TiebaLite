f092d70cafbeb2222716781194008aa7fbcfeab9
0ranko0P <ranko0p@outlook.com>
2025-11-02 20:38:42 +0800

    fix: Show type on gson deserialization failure

--- app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/converter/gson/GsonResponseBodyConverter.java
+++ app/src/main/java/com/huanchengfly/tieba/post/api/retrofit/converter/gson/GsonResponseBodyConverter.java
@@ -2,11 +2,13 @@ package com.huanchengfly.tieba.post.api.retrofit.converter.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonIOException;
+import com.google.gson.JsonParseException;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
@@ -14,23 +16,25 @@ import retrofit2.Converter;
 final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
     private final Gson gson;
     private final TypeAdapter<T> adapter;
+    private final Type type;
 
-    GsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+    GsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter, Type type) {
         this.gson = gson;
         this.adapter = adapter;
+        this.type = type;
     }
 
     @Override
     public T convert(ResponseBody value) throws IOException {
         JsonReader jsonReader = gson.newJsonReader(value.charStream());
-        try {
+        try (value) {
             T result = adapter.read(jsonReader);
             if (jsonReader.peek() != JsonToken.END_DOCUMENT) {
                 throw new JsonIOException("JSON document was not fully consumed.");
             }
             return result;
-        } finally {
-            value.close();
+        } catch (Throwable e) {
+            throw new JsonParseException("Convert " + type + " failed: ",  e);
         }
     }
 }
