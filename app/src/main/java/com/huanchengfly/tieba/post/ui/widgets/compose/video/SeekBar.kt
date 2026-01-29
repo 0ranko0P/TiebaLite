d426bb8ff5457b4cb20bb360505d795321733116
0ranko0P <22423336+0ranko0P@users.noreply.github.com>
2026-01-29 22:01:02 +0800

    feat: SeekBar: 性能优化

--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/video/SeekBar.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/video/SeekBar.kt
@@ -26,15 +26,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastRoundToInt
 import com.huanchengfly.tieba.post.ui.widgets.compose.LinearProgressIndicator
@@ -89,26 +88,16 @@ fun SeekBar(
         Column {
             // SEEK POPUP
             if (onGoingDrag) {
-                var popupSize by remember { mutableStateOf(IntSize.Zero) }
-
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.spacedBy(8.dp),
                     modifier = Modifier
-                        .offset {
+                        .graphicsLayer {
+                            alpha = if (size == Size.Zero) 0f else 1f
                             // popup seeker must center the actual seeker position. Therefore, we offset
                             // it negatively to the left.
-                            IntOffset(
-                                x = (finalIndicatorOffset.x - popupSize.width / 2)
-                                    .coerceIn(0f, (boxWidth - popupSize.width)).fastRoundToInt(),
-                                y = 0
-                            )
-                        }
-                        .alpha(if (popupSize == IntSize.Zero) 0f else 1f)
-                        .onGloballyPositioned {
-                            if (popupSize != it.size) {
-                                popupSize = it.size
-                            }
+                            translationX = (finalIndicatorOffset.x - size.width / 2)
+                                .coerceIn(0f, (boxWidth - size.width))
                         }
                 ) {
                     Box(modifier = Modifier.shadow(4.dp)) {
