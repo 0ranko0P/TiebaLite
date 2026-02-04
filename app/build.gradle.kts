ffbe85293354927cf5f184e806d6797d91cab11a
0ranko0P <22423336+0ranko0P@users.noreply.github.com>
2026-02-04 21:46:59 +0800

    chore(release): 4.0.0 Beta 4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         //noinspection OldTargetApi
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 390858
-        versionName = "4.0.0 Beta 4"
+        versionCode = 390880
+        versionName = "4.0.0 Beta 4.1"
         // Configure custom runner to set up the Hilt test application
         testInstrumentationRunner = "$applicationId.TbLiteTestRunner"
         vectorDrawables {
