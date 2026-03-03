79d26d6b43376985e364eca99ae44fd91e23e5c4
0ranko0P <22423336+0ranko0P@users.noreply.github.com>
2026-03-03 22:02:36 +0800

    feat: ExplorePage: 重构DislikeBtn

--- app/src/main/java/com/huanchengfly/tieba/post/ui/page/main/explore/personalized/DislikeBtn.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/page/main/explore/personalized/DislikeBtn.kt
@@ -1,6 +1,5 @@
 package com.huanchengfly.tieba.post.ui.page.main.explore.personalized
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -8,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
 import androidx.compose.material3.ButtonDefaults
@@ -15,25 +15,23 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleButton
+import androidx.compose.material3.ToggleButtonDefaults
+import androidx.compose.material3.ToggleButtonShapes
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.huanchengfly.tieba.post.R
-import com.huanchengfly.tieba.post.ui.common.theme.compose.clickableNoIndication
 import com.huanchengfly.tieba.post.ui.common.theme.compose.onNotNull
 import com.huanchengfly.tieba.post.ui.models.explore.Dislike
 import com.huanchengfly.tieba.post.ui.widgets.compose.ClickMenu
 import com.huanchengfly.tieba.post.ui.widgets.compose.LocalHazeState
 import com.huanchengfly.tieba.post.ui.widgets.compose.VerticalGrid
-import com.huanchengfly.tieba.post.ui.widgets.compose.containerColor
-import com.huanchengfly.tieba.post.ui.widgets.compose.contentColor
 import com.huanchengfly.tieba.post.ui.widgets.compose.defaultHazeStyle
 import com.huanchengfly.tieba.post.ui.widgets.compose.defaultInputScale
 import com.huanchengfly.tieba.post.ui.widgets.compose.hazeSource
@@ -50,30 +48,32 @@ fun Dislike(
     onDislikeClicked: () -> Unit
 ) {
     val hazeState = LocalHazeState.current
-    val hazeInputScale = defaultInputScale()
     val menuState = rememberMenuState()
-    val buttonColors = ButtonDefaults.buttonColors(
-        containerColor = MaterialTheme.colorScheme.tertiary,
-        contentColor = MaterialTheme.colorScheme.onTertiary
-    )
 
     ClickMenu(
         menuContent = {
+            val toggleButtonShapes = ToggleButtonShapes(
+                shape = MaterialTheme.shapes.small,
+                pressedShape = MaterialTheme.shapes.medium,
+                checkedShape = CircleShape
+            )
+            val toggleButtonColors = ToggleButtonDefaults.toggleButtonColors(
+                containerColor = MaterialTheme.colorScheme.tertiaryContainer,
+                contentColor = MaterialTheme.colorScheme.onTertiaryContainer,
+            )
+
             Column(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
                     Text(
                         text = stringResource(id = R.string.title_dislike),
                         style = MaterialTheme.typography.titleMedium,
                         modifier = Modifier.weight(1f),
                     )
 
-                    SubmitButton(isEnabled = { selectedReasons.isNotEmpty() }) {
+                    SubmitButton {
                         dismiss()
                         onDislikeClicked()
                     }
@@ -88,33 +88,23 @@ fun Dislike(
                     items(
                         items = dislikeResource,
                         span = { if (it.id == 7) 2 else 1 }
-                    ) {
-                        val selected = it in selectedReasons
-                        val backgroundColor by animateColorAsState(
-                            targetValue = buttonColors.containerColor(selected)
-                        )
-
-                        Text(
-                            text = it.reason,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(backgroundColor, shape = MaterialTheme.shapes.small)
-                                .clickableNoIndication {
-                                    onDislikeSelected(it)
-                                }
-                                .padding(vertical = 8.dp, horizontal = 16.dp),
-                            color = buttonColors.contentColor(selected),
-                            textAlign = TextAlign.Center,
-                            style = MaterialTheme.typography.titleMedium,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
+                    ) { dislike ->
+                        ToggleButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            checked = dislike in selectedReasons,
+                            onCheckedChange = { onDislikeSelected(dislike) },
+                            shapes = toggleButtonShapes,
+                            colors = toggleButtonColors,
+                        ) {
+                            Text(text = dislike.reason, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                        }
                     }
                 }
             }
         },
         modifier = Modifier
             .onNotNull(hazeState) {
+                val hazeInputScale = defaultInputScale()
                 hazeSource(state = it, zIndex = 1f)
                     .hazeEffect(state = it, style = defaultHazeStyle()) {
                         inputScale = hazeInputScale
@@ -137,18 +127,17 @@ fun Dislike(
 }
 
 @Composable
-private fun SubmitButton(modifier: Modifier = Modifier, isEnabled: () -> Boolean, onClick: () -> Unit) {
+private fun SubmitButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
     val colors = ButtonDefaults.filledTonalButtonColors()
-    val enabled = isEnabled()
 
     Text(
-        text = stringResource(id = R.string.button_submit),
+        text = stringResource(id = R.string.button_submit_dislike),
         modifier = modifier
             .clip(MaterialTheme.shapes.small)
-            .background(color = colors.containerColor(enabled))
-            .clickable(enabled = enabled, onClick = onClick)
-            .padding(vertical = 4.dp, horizontal = 8.dp),
-        color = colors.contentColor(enabled),
+            .background(color = colors.containerColor)
+            .clickable(onClick = onClick)
+            .padding(vertical = 4.dp, horizontal = 10.dp),
+        color = colors.contentColor,
         fontWeight = FontWeight.Bold,
         style = MaterialTheme.typography.titleSmall,
     )
