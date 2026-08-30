01097861cf51d2a1c159363c89b21fd28b32b717
0ranko0P <22423336+0ranko0P@users.noreply.github.com>
2026-08-30 22:11:57 +0800

    fix: 设置夜间模式时被夜间主题按钮覆盖

--- app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/preference/SegmentedPrefsScreen.kt
+++ app/src/main/java/com/huanchengfly/tieba/post/ui/widgets/compose/preference/SegmentedPrefsScreen.kt
@@ -184,6 +184,7 @@ interface SettingsSegmentedPrefsScope<T>: SegmentedPrefsScope {
      * @param leadingIcon optional leading icon to be drawn at the beginning of the preference.
      * @param enabled controls the enabled state of this list pref. When `false`, this component will
      *   not respond to user input, and it will appear visually disabled.
+     * @param onValueChanged callback to be invoked when user selected new option in [options]
      */
     fun <K> listPref(
         property: KProperty1<T, K>,
@@ -194,6 +195,7 @@ interface SettingsSegmentedPrefsScope<T>: SegmentedPrefsScope {
         useSelectedAsSummary: Boolean = summary == null,
         leadingIcon: ImageVector? = null,
         enabled: Boolean = true,
+        onValueChanged: ((K) -> Unit)? = null,
     )
 }
 
@@ -449,6 +451,7 @@ private class SettingsSegmentedPrefsScopeImpl<T>(
         useSelectedAsSummary: Boolean,
         leadingIcon: ImageVector?,
         enabled: Boolean,
+        onValueChanged: ((K) -> Unit)?,
     ) {
         prefsItem(key = title, contentType = ItemType.Clickable) { shapes ->
             val context = LocalContext.current
@@ -468,6 +471,7 @@ private class SettingsSegmentedPrefsScopeImpl<T>(
                 enabled = enabled,
                 onValueChange = { newOption ->
                     this@SettingsSegmentedPrefsScopeImpl.saver.update(property, newOption)
+                    onValueChanged?.invoke(newOption)
                 },
             )
         }
